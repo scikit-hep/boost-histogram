a0152d8c36abdc7b509c9dc9140936c389dfe7e6
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2019-03-14 07:43:45 +0100

    Update license

--- src/histogram/storage.cpp
+++ src/histogram/storage.cpp
@@ -1,8 +1,7 @@
-// Copyright 2015-2018 Hans Dembinski and Henry Schreiner
+// Copyright 2018-2019 Henry Schreiner and Hans Dembinski
 //
-// Distributed under the Boost Software License, Version 1.0.
-// (See accompanying file LICENSE_1_0.txt
-// or copy at http://www.boost.org/LICENSE_1_0.txt)
+// Distributed under the 3-Clause BSD License.  See accompanying
+// file LICENSE or https://github.com/scikit-hep/boost-histogram for details.
 
 #include <boost/histogram/python/pybind11.hpp>
 
