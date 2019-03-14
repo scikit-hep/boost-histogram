480ccbe0cef3dada41dffb364fc244decd3d951d
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2019-03-14 23:32:38 +0100

    Storage and histogram cleanup

--- include/boost/histogram/python/storage.hpp
+++ include/boost/histogram/python/storage.hpp
@@ -7,9 +7,9 @@
 #pragma once
 
 #include <boost/histogram.hpp>
-#include <vector>
 #include <cstdint>
 
 namespace bh = boost::histogram;
 
-using vector_int_storage = bh::storage_adaptor<std::vector<uint64_t>>;
+using dense_int_storage = bh::dense_storage<uint64_t>;
+using dense_double_storage = bh::dense_storage<double>;
--- src/histogram/storage.cpp
+++ src/histogram/storage.cpp
@@ -21,20 +21,35 @@
 namespace bh = boost::histogram;
 
 void register_storage(py::module &m) {
-    
+
     py::module storage = m.def_submodule("storage");
-    
-    py::class_<bh::unlimited_storage<>>(storage, "unlimited", "Default adaptive storage")
-    .def(py::init<>(), "Default constructor")
+
+    // Fast storages
+
+    py::class_<dense_int_storage>(storage, "dense_int", "Integers in vectors storage type")
+    .def(py::init<>())
     ;
-    
-    py::class_<vector_int_storage>(storage, "vector", "Integers in vectors storage type")
-    .def(py::init<>(), "Default constructor")
+
+    py::class_<dense_double_storage>(storage, "dense_double", "Weighted storage without variance type (fast but simple)")
+    .def(py::init<>())
     ;
-    
-    py::class_<bh::weight_storage>(storage, "weight", "Weighted storage type")
-    .def(py::init<>(), "Default constructor")
+
+    // Default storages
+
+    py::class_<bh::unlimited_storage<>>(storage, "unlimited", "Pptimized for unweighted histograms, adaptive")
+    .def(py::init<>())
     ;
-    
-    
+
+    py::class_<bh::weight_storage>(storage, "weight", "Dense storage which tracks sums of weights and a variance estimate")
+    .def(py::init<>())
+    ;
+
+    py::class_<bh::profile_storage>(storage, "profile", "Dense storage which tracks means of samples in each cell")
+    .def(py::init<>())
+    ;
+
+    py::class_<bh::weighted_profile_storage>(storage, "weighted_profile", "Dense storage which tracks means of weighted samples in each cell")
+    .def(py::init<>())
+    ;
+
 }
