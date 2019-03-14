0796bc8cd58ceab879e51300b6a3deca43f9987e
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2019-03-14 23:32:38 +0100

    Storage and histogram cleanup

--- src/histogram/histogram.cpp
+++ src/histogram/histogram.cpp
@@ -28,30 +28,30 @@ namespace bh = boost::histogram;
 
 template<typename A, typename S>
 py::class_<bh::histogram<A, S>> register_histogram_by_type(py::module& m, const char* name, const char* desc) {
-    
+
     using histogram_t = bh::histogram<A, S>;
-   
+
     py::class_<histogram_t> hist(m, name, desc, py::buffer_protocol());
-    
+
     hist
     .def(py::init<const A&, S>(), "axes"_a, "storage"_a=S())
-    
+
     .def_buffer([](bh::histogram<A, S>& h) -> py::buffer_info
         {return make_buffer(h);})
-    
+
     .def("rank", &histogram_t::rank,
          "Number of axes (dimensions) of histogram" )
     .def("size", &histogram_t::size,
          "Total number of bins in the histogram (including underflow/overflow)" )
     .def("reset", &histogram_t::reset,
          "Reset bin counters to zero")
-    
+
     .def(py::self + py::self)
     .def(py::self == py::self)
     .def(py::self != py::self)
     .def(py::self *= double())
     .def(py::self /= double())
-    
+
     .def("axis",
         [](histogram_t &self, unsigned i){return self.axis(i);},
      "Get N-th axis with runtime index")
@@ -68,55 +68,78 @@ py::class_<bh::histogram<A, S>> register_histogram_by_type(py::module& m, const 
         return self.at(int_args);
         },
          "Access bin counter at indices")
-    
+
     .def("__repr__", [](histogram_t &self){
         std::ostringstream out;
         out << self;
         return out.str();
     })
-    
+
     ;
-    
+
     return hist;
 }
 
 void register_histogram(py::module& m) {
     py::module hist = m.def_submodule("hist");
-    
-    register_histogram_by_type<axes::regular, bh::default_storage>(hist,
-        "regular_unlimited",
-        "N-dimensional histogram for real-valued data.");
-    
-    register_histogram_by_type<axes::regular, vector_int_storage>(hist,
-        "regular_int",
-        "N-dimensional histogram for int-valued data.");
-    
-    register_histogram_by_type<axes::regular_noflow, vector_int_storage>(hist,
-        "regular_int_noflow",
-        "N-dimensional histogram for int-valued data.");
-    
-    register_histogram_by_type<axes::regular, bh::weight_storage>(hist,
-        "regular_weight",
-        "N-dimensional histogram for real-valued data with weights.");
-    
-    register_histogram_by_type<axes::any, vector_int_storage>(hist,
+
+    // Completely general histograms
+
+    register_histogram_by_type<axes::any, dense_double_storage>(hist,
+        "any_double",
+        "N-dimensional histogram for real-valued data with weights with any axis types.");
+
+    register_histogram_by_type<axes::any, dense_int_storage>(hist,
         "any_int",
         "N-dimensional histogram for int-valued data with any axis types.");
-    
-    register_histogram_by_type<axes::regular_1D, vector_int_storage>(hist,
+
+    register_histogram_by_type<axes::any, bh::unlimited_storage<>>(hist,
+        "any_unlimited",
+        "N-dimensional histogram for unlimited size data with any axis types.");
+
+    register_histogram_by_type<axes::any, bh::weight_storage>(hist,
+        "any_weight",
+        "N-dimensional histogram for weighted data with any axis types.");
+
+    // register_histogram_by_type<axes::any, bh::profile_storage>(hist,
+    //    "any_profile",
+    //    "N-dimensional histogram for sampled data with any axis types.");
+
+    // register_histogram_by_type<axes::any, bh::weighted_profile_storage>(hist,
+    //    "any_weighted_profile",
+    //    "N-dimensional histogram for weighted and sampled data with any axis types.");
+
+    // Fast specializations - uniform types
+
+    register_histogram_by_type<axes::regular, bh::unlimited_storage<>>(hist,
+        "regular_unlimited",
+        "N-dimensional histogram for real-valued data.");
+
+    register_histogram_by_type<axes::regular, dense_int_storage>(hist,
+        "regular_int",
+        "N-dimensional histogram for int-valued data.");
+
+    register_histogram_by_type<axes::regular_noflow, dense_int_storage>(hist,
+        "regular_int_noflow",
+        "N-dimensional histogram for int-valued data.");
+
+    // Fast specializations: Fixed number of axis (may be removed if above versions are fast enough)
+    // Mostly targeting histogram styles supported by numpy for these max performance versions.
+
+    register_histogram_by_type<axes::regular_1D, dense_int_storage>(hist,
         "regular_int_1d",
         "1-dimensional histogram for int valued data.");
-    
-    register_histogram_by_type<axes::regular_2D, vector_int_storage>(hist,
+
+    register_histogram_by_type<axes::regular_2D, dense_int_storage>(hist,
         "regular_int_2d",
         "2-dimensional histogram for int valued data.");
-    
-    register_histogram_by_type<axes::regular_noflow_1D, vector_int_storage>(hist,
+
+    register_histogram_by_type<axes::regular_noflow_1D, dense_int_storage>(hist,
         "regular_int_noflow_1d",
         "1-dimensional histogram for int valued data.");
-    
-    register_histogram_by_type<axes::regular_noflow_2D, vector_int_storage>(hist,
+
+    register_histogram_by_type<axes::regular_noflow_2D, dense_int_storage>(hist,
         "regular_int_noflow_2d",
         "2-dimensional histogram for int valued data.");
-    
+
 }
