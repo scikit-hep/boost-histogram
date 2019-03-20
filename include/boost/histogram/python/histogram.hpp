51abf86107c4be84fddbd10361784184c8f6d72f
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2019-03-19 21:31:52 -0400

    Updating to latest master of Boost Histogram

--- include/boost/histogram/python/histogram.hpp
+++ include/boost/histogram/python/histogram.hpp
@@ -24,7 +24,7 @@ py::buffer_info make_buffer(bh::histogram<A, S>& h) {
     ssize_t factor = sizeof(in_storage_t);
     
     for (unsigned i=0; i<rank; i++) {
-        auto dim = bh::axis::traits::extend(h.axis(i)); // (under/overflow)
+        auto dim = bh::axis::traits::extent(h.axis(i)); // (under/overflow)
         diminsions.push_back(dim);
         strides.push_back(factor);
         factor *= dim;
