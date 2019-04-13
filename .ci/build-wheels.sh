ed466e347aa64cafc9d29af15624deb545ee7e85
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2019-04-13 23:14:55 +0200

    Starting manylinux work

---
+++ .ci/build-wheels.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e -x
+
+# Compile wheels
+for PYBIN in /opt/python/*/bin; do
+    "${PYBIN}/pip" install -r /io/dev-requirements.txt
+    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/*.whl; do
+    auditwheel repair "$whl" -w /io/wheelhouse/
+done
+
+# Install packages and test
+for PYBIN in /opt/python/*/bin/; do
+    "${PYBIN}/pip" install python-manylinux-demo --no-index -f /io/wheelhouse
+    (cd "$HOME"; "${PYBIN}/python" -m pytest pymanylinuxdemo)
+done
