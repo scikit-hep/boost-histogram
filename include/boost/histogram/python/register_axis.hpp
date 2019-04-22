608ec4bea0ea6bc301096cd12b2b0e7b57dd261c
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2019-04-22 11:27:19 +0200

    IV is now automatic for axes

--- include/boost/histogram/python/register_axis.hpp
+++ include/boost/histogram/python/register_axis.hpp
@@ -27,9 +27,29 @@
 
 using namespace std::literals;
 
+
+/// Add helpers common to all types with a range of values
+template <typename A>
+py::class_<bh::axis::interval_view<A>> register_axis_iv(py::module &m, const char *name) {
+    using A_iv               = bh::axis::interval_view<A>;
+    py::class_<A_iv> axis_iv = py::class_<A_iv>(m, name, "Lightweight bin view");
+    
+    axis_iv.def("upper", &A_iv::upper)
+    .def("lower", &A_iv::lower)
+    .def("center", &A_iv::center)
+    .def("width", &A_iv::width)
+    .def(py::self == py::self)
+    .def(py::self != py::self)
+    .def("__repr__", [](const A_iv &self) {
+        return "<bin ["s + std::to_string(self.lower()) + ", "s + std::to_string(self.upper()) + "]>"s;
+    });
+    
+    return axis_iv;
+}
+
 /// Add items to an axis where the axis values are continious
 template <typename A, typename B>
-void add_to_axis(B &&axis, std::false_type) {
+void add_to_axis(py::module m, const char* name, B &&axis, std::false_type) {
     axis.def("bin", &A::bin, "The bin details (center, lower, upper)", "idx"_a, py::keep_alive<0, 1>());
     axis.def(
         "bins", [](const A &self, bool flow) { return axis_to_bins(self, flow); }, "flow"_a = false);
@@ -50,11 +70,15 @@ void add_to_axis(B &&axis, std::false_type) {
             return centers;
         },
         "Return the bin centers");
+    
+    std::string name_iv = "_"s + name + "_iv"s;
+    
+    register_axis_iv<A>(m, name_iv.c_str());
 }
 
 /// Add items to an axis where the axis values are not continious (categories of strings, for example)
 template <typename A, typename B>
-void add_to_axis(B &&axis, std::true_type) {
+void add_to_axis(py::module, const char*, B &&axis, std::true_type) {
     axis.def("bin", &A::bin, "The bin name", "idx"_a);
     axis.def(
         "bins", [](const A &self, bool flow) { return axis_to_bins(self, flow); }, "flow"_a = false);
@@ -65,8 +89,8 @@ void add_to_axis(B &&axis, std::true_type) {
 
 /// Add helpers common to all axis types
 template <typename A, typename... Args>
-py::class_<A> register_axis(py::module &m, Args &&... args) {
-    py::class_<A> axis(m, std::forward<Args>(args)...);
+py::class_<A> register_axis(py::module &m, const char* name, Args &&... args) {
+    py::class_<A> axis(m, name, std::forward<Args>(args)...);
 
     // using value_type = decltype(A::value(1.0));
 
@@ -113,7 +137,7 @@ py::class_<A> register_axis(py::module &m, Args &&... args) {
     using Result = decltype(std::declval<A>().bin(std::declval<int>()));
 
     // This is a replacement for constexpr if
-    add_to_axis<A>(
+    add_to_axis<A>(m, name,
         axis, std::integral_constant < bool, std::is_reference<Result>::value || std::is_integral<Result>::value > {});
 
     axis.def(make_pickle<A>());
@@ -121,21 +145,3 @@ py::class_<A> register_axis(py::module &m, Args &&... args) {
     return axis;
 }
 
-/// Add helpers common to all types with a range of values
-template <typename A>
-py::class_<bh::axis::interval_view<A>> register_axis_iv_by_type(py::module &m, const char *name) {
-    using A_iv               = bh::axis::interval_view<A>;
-    py::class_<A_iv> axis_iv = py::class_<A_iv>(m, name, "Lightweight bin view");
-
-    axis_iv.def("upper", &A_iv::upper)
-        .def("lower", &A_iv::lower)
-        .def("center", &A_iv::center)
-        .def("width", &A_iv::width)
-        .def(py::self == py::self)
-        .def(py::self != py::self)
-        .def("__repr__", [](const A_iv &self) {
-            return "<bin ["s + std::to_string(self.lower()) + ", "s + std::to_string(self.upper()) + "]>"s;
-        });
-
-    return axis_iv;
-}
