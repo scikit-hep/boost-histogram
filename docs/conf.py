6d7507b6de87312306016907051fba03b7beffbc
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2019-04-23 16:06:56 +0200

    Adding doc requirements
    [skip ci]

--- docs/conf.py
+++ docs/conf.py
@@ -54,6 +54,8 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# Read the Docs needs this explicitly listed.
+master_doc = 'index'
 
 # -- Options for HTML output -------------------------------------------------
 
