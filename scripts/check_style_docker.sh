5befd67788d9cf25ff5bb85f7ab198aef92f3c98
Henry Schreiner <henry.fredrick.schreiner@cern.ch>
2019-05-16 22:25:47 +0200

    Docker user and group ID fix (#70)
    * Docker user and group ID fix
    
    * Adding pytest runner

--- scripts/check_style_docker.sh
+++ scripts/check_style_docker.sh
@@ -6,7 +6,7 @@ CLANG_FORMAT=unibeautify/clang-format
 set -evx
 
 docker run --rm ${CLANG_FORMAT} --version
-docker run --rm -v "$(pwd)":/workdir -w /workdir ${CLANG_FORMAT} -style=file -sort-includes -i $(git ls-files -- '*.cpp' '*.hpp' '*.cu' '*.h')
+docker run --rm --user=$(id -u):$(id -g) -v "$(pwd)":/workdir -w /workdir ${CLANG_FORMAT} -style=file -sort-includes -i $(git ls-files -- '*.cpp' '*.hpp' '*.cu' '*.h')
 
 git diff --exit-code --color
 
