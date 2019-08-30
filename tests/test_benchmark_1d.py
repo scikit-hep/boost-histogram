2948dab228c52756e813085eabbd001f95540be4
Henry Schreiner <hschrein@cern.ch>
2019-08-30 17:05:55 -0400

    Fixing 1D benchmarks
    [skip ci]

--- tests/test_benchmark_1d.py
+++ tests/test_benchmark_1d.py
@@ -4,7 +4,7 @@ import numpy as np
 import boost.histogram as bh
 from numpy.testing import assert_array_equal, assert_allclose
 
-from boost.histogram.axis import regular_uoflow, regular_noflow
+from boost.histogram.axis import regular
 
 bins=100
 ranges=(-1,1)
@@ -23,29 +23,14 @@ def test_numpy_perf_1d(benchmark):
     result, _ = benchmark(np.histogram, vals, bins=bins, range=ranges)
     assert_array_equal(result, answer)
 
-def make_and_run_hist(hist, axes, vals, fill):
-    histo = hist(axes)
-    if fill is None:
-        histo.fill(vals)
-    elif fill < 0:
-        histo.fill(vals, atomic=-fill)
-    else:
-        histo.fill(vals, threads=fill)
-
+def make_and_run_hist(flow):
+    histo = bh.histogram(regular(bins, *ranges, flow=flow))
+    histo.fill(vals)
     return histo.view()
 
-histax = (
-        (bh.hist.any_int, regular_uoflow, None),
-        (bh.hist.any_int, regular_noflow, None),
-        (bh.hist.regular_int, regular_uoflow, None),
-        (bh.hist.regular_noflow_int, regular_noflow, None),
-        (bh.hist.regular_atomic_int, regular_uoflow, None),
-        (bh.hist.regular_atomic_int, regular_uoflow, -4),
-        (bh.hist.regular_int, regular_uoflow, 4),
-        )
 
 @pytest.mark.benchmark(group='1d-fills')
-@pytest.mark.parametrize("hist, axis, fill", histax)
-def test_1d(benchmark, hist, axis, fill):
-    result = benchmark(make_and_run_hist, hist, [axis(bins, *ranges)], vals, fill)
+@pytest.mark.parametrize("flow", [True, False])
+def test_1d(benchmark, flow):
+    result = benchmark(make_and_run_hist, flow)
     assert_allclose(result[:-1], answer[:-1], atol=2)
