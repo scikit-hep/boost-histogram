cc95a32dee477e2813da469b41eeae72557d1320
Henry Schreiner <hschrein@cern.ch>
2019-10-11 15:10:26 -0400

    Moving indexing to Python

--- boost_histogram/hist.py
+++ boost_histogram/hist.py
@@ -42,3 +42,92 @@ def _make_histogram(*args, **kwargs):
 
 
 histogram = FactoryMeta(_make_histogram, tuple(_histogram_and_storage.values()))
+
+
+def _expand_ellipsis(indexes, rank):
+    indexes = list(indexes)
+    number_ellipses = indexes.count(Ellipsis)
+    if number_ellipses == 0:
+        return indexes
+    elif number_ellipses == 1:
+        index = indexes.index(Ellipsis)
+        additional = rank + 1 - len(indexes)
+        if additional < 0:
+            raise IndexError("too many indices for histogram")
+
+        # Fill out the ellipsis with empty slices
+        return indexes[:index] + [slice(None)] * additional + indexes[index + 1 :]
+
+    else:
+        raise IndexError("an index can only have a single ellipsis ('...')")
+
+
+def _compute_getitem(self, index):
+    # Normalize -> h[i] == h[i,]
+    if not isinstance(index, tuple):
+        index = (index,)
+
+    # Now a list
+    indexes = _expand_ellipsis(index, self.rank)
+
+    # Allow [bh.loc(...)] to work
+    for i in range(len(indexes)):
+        if hasattr(indexes[i], "value"):
+            indexes[i] = self.axis(i).index(indexes[i].value)
+
+    if len(indexes) != self.rank:
+        raise IndexError("IndexError: Wrong number of indices for histogram")
+
+    # If this is (now) all integers, return the bin contents
+    try:
+        return self.at(*indexes)
+    except RuntimeError:
+        pass
+
+    projections = []
+    slices = []
+
+    # Compute needed slices and projections
+    for i, ind in enumerate(indexes):
+        if not isinstance(ind, slice):
+            raise IndexError(
+                "Invalid arguments as an index, use all integers "
+                "or all slices, and do not mix"
+            )
+        if ind != slice(None):
+            merge = 1
+            if ind.step is not None:
+                if hasattr(ind.step, "projection"):
+                    if ind.step.projection:
+                        projections.append(i)
+                        if ind.start is not None or ind.stop is not None:
+                            raise IndexError(
+                                "Currently cut projections are not supported"
+                            )
+                    elif hasattr(ind.step, "factor"):
+                        merge = ind.step.factor
+                    else:
+                        raise IndexError("Invalid rebin, must have integer .factor")
+                else:
+                    raise IndexError(
+                        "The third argument to a slice must be rebin or projection"
+                    )
+
+            process_loc = (
+                lambda x, y: y
+                if x is None
+                else (self.axis(i).index(x.value) if hasattr(x, "value") else x)
+            )
+            begin = process_loc(ind.start, 0)
+            end = process_loc(ind.stop, len(self.axis(i)))
+
+            slices.append(_core.algorithm.slice_and_rebin(i, begin, end, merge))
+
+    reduced = self.reduce(*slices)
+    return reduced.project(*projections) if projections else reduced
+
+
+for h in _histogram_and_storage.values():
+    h.__getitem__ = _compute_getitem
+
+del h
--- setup.py
+++ setup.py
@@ -42,7 +42,6 @@ SRC_FILES = [
     "src/register_version.cpp",
     "src/register_algorithm.cpp",
     "src/register_axis.cpp",
-    "src/register_shared_histogram.cpp",
     "src/register_histograms.cpp",
     "src/register_storage.cpp",
     "src/register_accumulators.cpp",
