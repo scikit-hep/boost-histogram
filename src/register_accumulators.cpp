420999ed1600b052ed2599b84404ae96b304350e
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2019-10-11 23:08:24 -0400

    Adding accumulator fill and kw arguments

--- src/register_accumulators.cpp
+++ src/register_accumulators.cpp
@@ -10,8 +10,46 @@
 #include <boost/histogram/accumulators/sum.hpp>
 #include <boost/histogram/accumulators/weighted_mean.hpp>
 #include <boost/histogram/accumulators/weighted_sum.hpp>
+#include <boost/histogram/python/kwargs.hpp>
 #include <boost/histogram/python/register_accumulator.hpp>
 #include <pybind11/operators.h>
+
+/// The mean fill can be implemented once. (sum fill varies slightly)
+template <class T>
+decltype(auto) make_mean_fill() {
+    return [](T &self, py::object value, py::kwargs kwargs) {
+        py::object weight = optional_arg(kwargs, "weight", py::none());
+        finalize_args(kwargs);
+        if(weight.is_none()) {
+            py::vectorize([](T &self, double val) {
+                self(val);
+                return false;
+            })(self, value);
+        } else {
+            py::vectorize([](T &self, double wei, double val) {
+                self(wei, val);
+                return false;
+            })(self, weight, value);
+        }
+        return self;
+    };
+}
+
+/// The mean call can be implemented once. (sum uses +=)
+template <class T>
+decltype(auto) make_mean_call() {
+    return [](T &self, double value, py::kwargs kwargs) {
+        py::object weight = optional_arg(kwargs, "weight", py::none());
+        finalize_args(kwargs);
+
+        if(weight.is_none())
+            self(value);
+        else
+            self(py::cast<double>(weight), value);
+
+        return self;
+    };
+}
 
 void register_accumulators(py::module &accumulators) {
     using weighted_sum = bh::accumulators::weighted_sum<double>;
@@ -26,6 +64,27 @@ void register_accumulators(py::module &accumulators) {
 
         .def(py::self += double())
 
+        .def(
+            "fill",
+            [](weighted_sum &self, py::object value, py::kwargs kwargs) {
+                py::object variance = optional_arg(kwargs, "variance", py::none());
+                finalize_args(kwargs);
+                if(variance.is_none()) {
+                    py::vectorize([](weighted_sum &self, double val) {
+                        self += val;
+                        return false;
+                    })(self, value);
+                } else {
+                    py::vectorize([](weighted_sum &self, double val, double var) {
+                        self += weighted_sum(val, var);
+                        return false;
+                    })(self, value, variance);
+                }
+                return self;
+            },
+            "value"_a,
+            "Fill the accumulator with values. Optional variance parameter.")
+
         ;
 
     using sum = bh::accumulators::sum<double>;
@@ -37,6 +96,19 @@ void register_accumulators(py::module &accumulators) {
             "value", &sum::operator double, [](sum &s, double v) { s = v; })
 
         .def(py::self += double())
+
+        .def(
+            "fill",
+            [](sum &self, py::object value) {
+                py::vectorize([](sum &self, double v) {
+                    self += v;
+                    return false; // Required in PyBind11 2.4.2,
+                                  // requirement may be removed
+                })(self, value);
+                return self;
+            },
+            "value"_a,
+            "Run over an array with the accumulator")
 
         .def_property_readonly("small", &sum::small)
         .def_property_readonly("large", &sum::large)
@@ -56,21 +128,15 @@ void register_accumulators(py::module &accumulators) {
         .def_property_readonly("variance", &weighted_mean::variance)
         .def_property_readonly("value", &weighted_mean::value)
 
-        .def(
-            "__call__",
-            [](weighted_mean &self, double value) {
-                self(value);
-                return self;
-            },
-            "value"_a)
-        .def(
-            "__call__",
-            [](weighted_mean &self, double weight, double value) {
-                self(weight, value);
-                return self;
-            },
-            "weight"_a,
-            "value"_a)
+        .def("__call__",
+             make_mean_call<weighted_mean>(),
+             "value"_a,
+             "Fill with value and optional keyword-only weight")
+
+        .def("fill",
+             make_mean_fill<weighted_mean>(),
+             "value"_a,
+             "Fill the accumulator with values. Optional weight parameter.")
 
         ;
 
@@ -86,21 +152,15 @@ void register_accumulators(py::module &accumulators) {
         .def_property_readonly("variance", &mean::variance)
         .def_property_readonly("value", &mean::value)
 
-        .def(
-            "__call__",
-            [](mean &self, double value) {
-                self(value);
-                return self;
-            },
-            "value"_a)
-        .def(
-            "__call__",
-            [](mean &self, double weight, double value) {
-                self(weight, value);
-                return self;
-            },
-            "weight"_a,
-            "value"_a)
+        .def("__call__",
+             make_mean_call<mean>(),
+             "value"_a,
+             "Fill with value and optional keyword-only weight")
+
+        .def("fill",
+             make_mean_fill<mean>(),
+             "value"_a,
+             "Fill the accumulator with values. Optional weight parameter.")
 
         ;
 }
