00f12c373267ff55be1ec063a976f909199be5c7
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2019-10-31 15:42:33 -0700

    Working cpp module and tests

---
+++ boost_histogram/cpp/__init__.py
@@ -0,0 +1,5 @@
+# TODO: Eventually this should be listed explicitly, but this
+# is less likely to break during heavy development
+from .. import *
+
+from .._internal.hist import BoostHistogram as histogram
---
+++ tests/test_boost_histogram.py
@@ -0,0 +1,29 @@
+# You can enter maximum compatibility mode by typing:
+# import boost_histogram.cpp as bh
+# However, this is a test so we will import both.
+import boost_histogram as bh
+import boost_histogram.cpp as bhc
+import pytest
+
+
+def test_usage_bh():
+    h = bhc.histogram(bhc.axis.regular(10, 0, 1), bhc.axis.category(["one", "two"]))
+    assert h.axis(0) == bhc.axis.regular(10, 0, 1)
+    assert h.axis(1) == bhc.axis.category(["one", "two"])
+
+    h(0, "one")
+
+    assert h.at(0, 0) == 1.0
+
+
+def test_convert_bh():
+    h = bhc.histogram(
+        bh.histogram(bh.axis.regular(10, 0, 1), bh.axis.category(["one", "two"]))
+    )
+    assert hasattr(h, "axis")
+    assert not hasattr(h, "axes")
+
+    h = bh.histogram(h)
+
+    assert not hasattr(h, "axis")
+    assert hasattr(h, "axes")
