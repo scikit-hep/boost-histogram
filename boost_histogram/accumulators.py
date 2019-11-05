8721d044045b7f8fb57d09aa85a8b008300eb30f
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2019-11-06 08:28:35 +1030

    Rename accumulators

--- boost_histogram/accumulators.py
+++ boost_histogram/accumulators.py
@@ -2,4 +2,9 @@ from __future__ import absolute_import, division, print_function
 
 del absolute_import, division, print_function
 
-from ._core.accumulators import sum, mean, weighted_sum, weighted_mean
+from ._core.accumulators import (
+    sum as Sum,
+    mean as Mean,
+    weighted_sum as WeightedSum,
+    weighted_mean as WeightedMean,
+)
