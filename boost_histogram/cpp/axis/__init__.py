98501b6602f533d994f28cff923ceb195a4c8e05
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2019-11-05 17:51:04 +1030

    Better cpp mode, fix final warnings

---
+++ boost_histogram/cpp/axis/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import, division, print_function
+
+del absolute_import, division, print_function
+
+from ...axis import (
+    options,
+    Regular as regular,
+    Variable as variable,
+    Integer as integer,
+    Category as category,
+)
+
+from . import transform
