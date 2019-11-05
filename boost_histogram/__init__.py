1830605d6470a105d88174d0f7666f18aeae9a4f
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2019-11-05 11:57:44 +1030

    Use AxisTransform

--- boost_histogram/__init__.py
+++ boost_histogram/__init__.py
@@ -2,13 +2,14 @@ from __future__ import absolute_import, division, print_function
 
 del absolute_import, division, print_function
 
-from ._internal.hist import Histogram as histogram
+from ._internal.hist import Histogram
 
 from . import axis, storage, accumulators, algorithm, numpy
 
 from .tag import loc, rebin, sum, underflow, overflow
 
-# Workaround for bh.project being available
+# Workarounds for smooth transitions from 0.5 series. Will be removed after 0.6.
 project = sum
+histogram = Histogram
 
 from .version import __version__
---
+++ boost_histogram/axis/transform.py
@@ -0,0 +1,5 @@
+from __future__ import absolute_import, division, print_function
+
+del absolute_import, division, print_function
+
+from .._internal.axis_transform import AxisTransform, Log, Sqrt, Pow
