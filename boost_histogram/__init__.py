2b7858a8dc054ecec82c9ad93d63c9cd665e6f79
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2019-11-05 14:56:32 +1030

    Fix issue with unlinking files

--- boost_histogram/__init__.py
+++ boost_histogram/__init__.py
@@ -9,7 +9,18 @@ from . import axis, storage, accumulators, algorithm, numpy
 from .tag import loc, rebin, sum, underflow, overflow
 
 # Workarounds for smooth transitions from 0.5 series. Will be removed after 0.6.
-project = sum
 histogram = Histogram
 
 from .version import __version__
+
+from .tag import project
+
+
+class histogram(Histogram):
+    __slots__ = ()
+
+    def __init__(self, *args, **kwargs):
+        import warnings
+
+        warnings.warn("Use Histogram instead", DeprecationWarning)
+        super(histogram, self).__init__(*args, **kwargs)
