474aedb5c80c46404edfdd406f42716270870a43
Henry Schreiner <henryschreineriii@gmail.com>
2020-01-14 12:58:15 -0500

    Dropping deprecated items

--- boost_histogram/storage.py
+++ boost_histogram/storage.py
@@ -24,31 +24,3 @@ from ._internal.storage import (
     Mean,
     WeightedMean,
 )
-
-from ._internal.utils import register as _register
-
-# Warnings to be removed after 0.6
-
-
-int = Int64()
-int._warning = "int"
-
-double = Double()
-double._warning = "double"
-
-unlimited = Unlimited()
-unlimited._warning = "unlimited"
-
-atomic_int = AtomicInt64()
-atomic_int._warning = "atomic_int"
-
-weight = Weight()
-weight._warning = "weight"
-
-mean = Mean()
-mean._warning = "mean"
-
-weighted_mean = WeightedMean()
-weighted_mean._warning = "weighted_mean"
-
-del _register
