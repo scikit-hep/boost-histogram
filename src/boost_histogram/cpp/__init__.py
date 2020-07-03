71697a51792144ff2dc1f9825552352cbd57ef17
Henry Schreiner <henryschreineriii@gmail.com>
2020-07-03 12:13:59 -0400

    chore: warn on cpp import

--- src/boost_histogram/cpp/__init__.py
+++ src/boost_histogram/cpp/__init__.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function
 
+import warnings
+
 from .._internal.hist import histogram
 
 from . import axis, storage, accumulators, algorithm
 from .. import __version__
 
 del absolute_import, division, print_function
+
+msg = "The cpp module has been deprecated, and will be removed after 0.8.0"
+warnings.warn(msg, FutureWarning)
 
 __all__ = ("histogram", "axis", "storage", "accumulators", "algorithm", "__version__")
