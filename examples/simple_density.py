ebfde9002a158341b6c6784ac1c53959ecd12c78
Henry Schreiner <henryschreineriii@gmail.com>
2020-07-09 23:38:20 -0400

    fix: warning when using np.prod in NumPy 1.19

--- examples/simple_density.py
+++ examples/simple_density.py
@@ -4,6 +4,8 @@
 import numpy as np
 import boost_histogram as bh
 import matplotlib.pyplot as plt
+import functools
+import operator
 
 # Make a 2D histogram
 hist = bh.Histogram(bh.axis.Regular(50, -3, 3), bh.axis.Regular(50, -3, 3))
@@ -12,7 +14,7 @@ hist = bh.Histogram(bh.axis.Regular(50, -3, 3), bh.axis.Regular(50, -3, 3))
 hist.fill(np.random.normal(size=1_000_000), np.random.normal(size=1_000_000))
 
 # Compute the areas of each bin
-areas = np.prod(hist.axes.widths, axis=0)
+areas = functools.reduce(operator.mul, hist.axes.widths)
 
 # Compute the density
 density = hist.view() / hist.sum() / areas
