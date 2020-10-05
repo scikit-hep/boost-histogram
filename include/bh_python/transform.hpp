6204f47f81b69ddcc408dc8c4d574f3f296a333e
Henry Schreiner <henry.fredrick.schreiner@cern.ch>
2020-10-05 12:00:41 -0400

    chore: cleanup and labeler (#463)
    * core: minor cleanup
    
    * ci: labeler

--- include/bh_python/transform.hpp
+++ include/bh_python/transform.hpp
@@ -71,8 +71,8 @@ struct func_transform {
             auto c = py::reinterpret_borrow<py::capsule>(
                 PyCFunction_GET_SELF(cfunc.ptr()));
 
-            // NOLINTNEXTLINE(google-readability-casting)
-            auto rec = (py::detail::function_record*)(c);
+            // This calls operator T* (PyCapsule_SetPointer)
+            auto rec = static_cast<py::detail::function_record*>(c);
 
             if(rec && rec->is_stateless
                && py::detail::same_type(
