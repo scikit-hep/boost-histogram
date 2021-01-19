fix: variance is variance of the mean, rather than variance of the sample
Don't return values if non valid

tests/test_plottable_protocol.py
@@ -5,10 +5,10 @@ import numpy as np
 from numpy.testing import assert_allclose
 
 
-COUNTS = [3, 4, 5, 6]
-VALUES = [1, 2, 3, 4]
-VARIANCES = [0.1, 0.1, 0.2, 0.2]
-SUM2 = [9, 8, 7, 6]  # not in protocol, so not checked
+COUNTS = np.array([3, 4, 5, 6])
+VALUES = np.array([1, 2, 3, 4])
+VARIANCES = np.array([0.1, 0.1, 0.2, 0.2])
+SUM2 = np.array([9, 8, 7, 6])  # not in protocol, so not checked
 
 
 def test_plottable_histogram_mean_int():
@@ -18,7 +18,7 @@ def test_plottable_histogram_mean_int():
 
     assert_allclose(COUNTS, h.counts())
     assert_allclose(VALUES, h.values())
-    assert_allclose(VARIANCES, h.variances())
+    assert_allclose(VARIANCES / COUNTS, h.variances())
 
     assert h.kind == bh.Kind.MEAN
     assert h.kind == "MEAN"