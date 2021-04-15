5ad0948c7ae98e8f0575f7aef881771a6b00a331
Henry Schreiner <henry.fredrick.schreiner@cern.ch>
2021-04-15 18:22:18 -0400

    docs: convert recommonmark to myst-parser (#551)
    * docs: remove recommonmark
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ extras = {
     "test": ["pytest", "pytest-benchmark", "typing_extensions", "cloudpickle"],
     "docs": [
         "Sphinx~=3.0",
-        "recommonmark>=0.5.0",
+        "myst_parser>=0.13",
         "sphinx_book_theme==0.38.0",
         "nbsphinx",
         "sphinx_copybutton",
