4c680196fcab8f2b0c62b31cc58e472c397bdf7f
Henry Schreiner <henryschreineriii@gmail.com>
2021-08-25 15:02:12 -0400

    ci: cross-compile for aarch64

--- setup.py
+++ setup.py
@@ -11,6 +11,77 @@ from pybind11.setup_helpers import ParallelCompile, Pybind11Extension  # noqa: E
 
 del sys.path[-1]
 
+
+def cross_compile_patch() -> None:  # noqa: C901
+    import os
+
+    target_arch = os.environ.get("TARGET_ARCH", None)
+    if target_arch:
+        import copy
+        import distutils
+        import distutils.sysconfig
+        import sysconfig
+
+        get_config_var_backup = sysconfig.get_config_var  # noqa: F841
+        get_platform_backup = sysconfig.get_platform
+        get_config_vars_backup = sysconfig.get_config_vars
+        distutils_get_config_vars_backup = distutils.sysconfig.get_config_vars
+
+        def get_platform():
+            out = get_platform_backup()
+            if target_arch is not None and isinstance(out, str):
+                original_out = out
+                out = out.replace("x86_64", target_arch)
+                print("   Replace %s -> %s", original_out, out)
+            return out
+
+        def get_config_vars(*args):
+            out = get_config_vars_backup(*args)
+            target_arch = os.environ.get("TARGET_ARCH", None)
+            if target_arch is None:
+                return out
+            out_xfix = copy.deepcopy(out)
+            for k, v in out.items():
+                if not (isinstance(v, str) and "x86_64" in v):
+                    continue
+                if k not in {"SO", "SOABI", "EXT_SUFFIX", "BUILD_GNU_TYPE"}:
+                    continue
+                fix = v.replace("x86_64", target_arch)
+                print("   Replace %s: %s -> %s", k, v, fix)
+                out_xfix[k] = fix
+            return out_xfix
+
+        def distutils_get_config_vars(*args):
+            out = distutils_get_config_vars_backup(*args)
+            target_arch = os.environ.get("TARGET_ARCH", None)
+            if target_arch is None:
+                return out
+            if isinstance(out, list):
+                fixes = []
+                for item in out:
+                    if not (isinstance(item, str) and "x86_64" in item):
+                        fixes.append(item)
+                    else:
+                        fixes.append(item.replace("x86_64", target_arch))
+                return fixes
+            out_xfix = copy.deepcopy(out)
+            for k, v in out.items():
+                if not (isinstance(v, str) and "x86_64" in v):
+                    continue
+                if k not in {"SO", "SOABI", "EXT_SUFFIX", "BUILD_GNU_TYPE"}:
+                    continue
+                fix = v.replace("x86_64", target_arch)
+                print("   Replace %s: %s -> %s", k, v, fix)
+                out_xfix[k] = fix
+            return out_xfix
+
+        sysconfig.get_platform = get_platform
+        sysconfig.get_config_vars = get_config_vars
+        distutils.sysconfig.get_config_vars = distutils_get_config_vars
+
+
+cross_compile_patch()
+
 # Use the environment variable CMAKE_BUILD_PARALLEL_LEVEL to control parallel builds
 ParallelCompile("CMAKE_BUILD_PARALLEL_LEVEL").install()
 
