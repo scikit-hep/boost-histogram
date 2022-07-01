e0c2f5adf7e6bf90ea1dc6b30a37485e5cd15d44
Jay Gohil <jay.gohil.info@gmail.com>
2022-07-01 14:57:24 -0400

    Removed colorized return text

--- src/boost_histogram/_internal/hist.py
+++ src/boost_histogram/_internal/hist.py
@@ -340,21 +340,16 @@ class Histogram:
 
     def compare(self, hist2: "Histogram") -> str:
         if not (np.allclose(self.view().shape, hist2.view().shape)):
-            return f"The histogram dimensions [\033[91m {str(self.view().shape)} and {str(hist2.view().shape)} \033[0m] are not equal."
+            return f"The histogram dimensions [{self.view().shape} and {hist2.view().shape}] are not equal."
         if not (np.allclose(self.view(), hist2.view())):
-            return f"The histogram contents : \n \033[91m {str(self.view())} \033[0m \nand\n \033[91m {str(hist2.view())} \033[0m \nare not equal."
+            return f"The histogram contents :\n {self.view()} \nand\n {hist2.view()} \nare not equal."
         if not (np.allclose(self.variances(), hist2.variances())):
-            return f"The histogram contents : \n \033[91m {str(self.variances())} \033[0m \nand\n \033[91m {str(hist2.variances())} \033[0m \nare not equal."
-        if not (
-            re.search("(?<=storage=).*", str(self.view))[0].split("(")[0]
-            == re.search("(?<=storage=).*", str(hist2.view))[0].split("(")[0]
-        ):
-            temp1, temp2 = str(
-                re.search("(?<=storage=).*", str(self.view))[0].split("(")[0]
-            ), str(re.search("(?<=storage=).*", str(hist2.view))[0].split("(")[0])
-            return f"The storage types [\033[91m {temp1} and {temp2} \033[0m] are not equal."
-        if not (list(self.axes) == list(hist2.axes)):
-            return f"The axes [\033[91m {str(list(self.axes))} and {str(list(hist2.axes))} \033[0m] are not equal."
+            return f"The histogram contents :\n {self.variances()} \nand\n {hist2.variances()} \nare not equal."
+        if not (re.search("(?<=storage=).*", str(self.view))[0].split('(')[0] == re.search("(?<=storage=).*", str(hist2.view))[0].split('(')[0]):
+            temp1, temp2 = re.search("(?<=storage=).*", str(self.view))[0].split('(')[0], re.search("(?<=storage=).*", str(hist2.view))[0].split('(')[0]
+            return f"The storage types ({temp1} and {temp2}) are not equal."
+        if not (list(self.axes)==list(hist2.axes)):
+            return f"The axes :\n {list(self.axes)} \nand\n {list(hist2.axes)} \nare not equal."
         return ""
 
     def view(
