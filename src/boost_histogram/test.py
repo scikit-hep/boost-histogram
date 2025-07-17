3a5d911aa48ab2de47cd8379324f54af455d34d4
Henry Schreiner <henryschreineriii@gmail.com>
2025-07-16 22:57:32 -0700

    chore: add more tests
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- src/boost_histogram/test.py
+++ src/boost_histogram/test.py
@@ -5,6 +5,7 @@ correctly. Run with `python -m boost_histogram.test`.
 
 from __future__ import annotations
 
+import pickle
 import unittest
 
 import boost_histogram as bh
@@ -20,6 +21,40 @@ class TestBoostHistogram(unittest.TestCase):
         self.assertEqual(h[3], 1)
         self.assertEqual(h[4], 0)
 
+    def test_2d_histogram(self) -> None:
+        h = bh.Histogram(bh.axis.Integer(4, 10), bh.axis.StrCategory(["a", "b", "c"], overflow=False), storage=bh.storage.Weight())
+        h.fill([5,5,5,7], ["a", "b", "b", "c"])
+        self.assertEqual(h[bh.loc(5), bh.loc("a")].value, 1)
+        self.assertEqual(h[bh.loc(5), bh.loc("b")].value, 2)
+        self.assertEqual(h[bh.loc(7), bh.loc("c")].value, 1)
+        self.assertEqual(h[bh.loc(8), bh.loc("c")].value, 0)
+
+    def test_pickle(self) -> None:
+        h = bh.Histogram(bh.axis.Variable([0, .3, .4, .5, 1.0]), storage=bh.storage.Mean())
+        h.fill([0.15, 0.25, 0.25, 0.35], sample=[1, 2, 3, 4])
+        pickled = pickle.dumps(h)
+        unpickled = pickle.loads(pickled)
+        self.assertEqual(h, unpickled)
+
+    def test_sqrt_transform(self) -> None:
+        a = bh.axis.Regular(10, 0, 10, transform=bh.axis.transform.sqrt)
+        # Edges: 0. ,  0.1,  0.4,  0.9,  1.6,  2.5,  3.6,  4.9,  6.4,  8.1, 10.
+
+        self.assertEqual(a.index(-100), 10)  # Always in overflow bin
+        self.assertEqual(a.index(-1), 10)    # When transform is invalid
+        self.assertEqual(a.index(0), 0)
+        self.assertEqual(a.index(0.15), 1)
+        self.assertEqual(a.index(0.5), 2)
+        self.assertEqual(a.index(1), 3)
+        self.assertEqual(a.index(1.7), 4)
+        self.assertEqual(a.index(9), 9)
+        self.assertEqual(a.index(11), 10)
+        self.assertEqual(a.index(1000), 10)
+
+        self.assertAlmostEqual(a.bin(0)[0], 0)
+        self.assertAlmostEqual(a.bin(1)[0], 0.1)
+        self.assertAlmostEqual(a.bin(1)[1], 0.4)
+        self.assertAlmostEqual(a.bin(2)[0], 0.4)
 
 if __name__ == "__main__":
     unittest.main("boost_histogram.test", warnings="error")
