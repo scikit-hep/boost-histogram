a05d5954e03821adbf00902d48d3b313a659bd68
Henry Schreiner <henry.fredrick.schreiner@cern.ch>
2026-01-15 15:02:24 -0500

    chore: setting __file__ no longer required (pybind11 3.0) (#1066)
    Signed-off-by: Henry Schreiner <henryfs@princeton.edu>

--- src/boost_histogram/histogram.py
+++ src/boost_histogram/histogram.py
@@ -74,16 +74,6 @@ def __dir__() -> list[str]:
     return __all__
 
 
-# Support cloudpickle - pybind11 submodules do not have __file__ attributes
-# And setting this in C++ causes a segfault
-_core.accumulators.__file__ = _core.__file__
-_core.algorithm.__file__ = _core.__file__
-_core.axis.__file__ = _core.__file__
-_core.axis.transform.__file__ = _core.__file__
-_core.hist.__file__ = _core.__file__
-_core.storage.__file__ = _core.__file__
-
-
 NOTHING = object()
 
 
